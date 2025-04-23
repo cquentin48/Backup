593649712527def3f7bad1c8f1bd23380d879d77
Quentin CHAPEL <chapelquen@gmail.com>
2025-04-23 12:02:02 +0200

    [Backend]Chat analysis (wip)

--- backend/chatbot/analysis.py
+++ backend/chatbot/analysis.py
@@ -1,4 +1,7 @@
 import regex as re
+
+from typing import List, Dict
+
 from text_to_num import alpha2digit
 
 REGEX_PATTERNS = {
@@ -6,19 +9,28 @@ REGEX_PATTERNS = {
     "CORES": r"([0-9]+\s*[cC](oeur|ore|œur)s?){1}"
 }
 
+# TODO: put every pattern into a for loop with the digitalized sentence before.
 
-def replace_values(sentence: str):
+def replace_values(sentence: str, entities: List[Dict]):
     for pattern, regex_pattern in REGEX_PATTERNS.items():
         for match in re.find(regex_pattern, sentence):
             sentence = sentence[:match.start()]+f"{{{pattern}}}"+sentence[match.end():]
+            entities.append({
+                "pattern": pattern,
+                "value": match
+            })
 
-def replace_count(sentence:str):
+def replace_count(sentence:str, entities: List[Dict]):
     sentence = alpha2digit(sentence).split(' ')
     tokenized_sentence = []
     for word in sentence:
         try:
-            int(word)
+            digital_value = int(word)
             tokenized_sentence.append("{COUNT}")
+            entities.append({
+                "pattern": "COUNT",
+                "value": digital_value
+            })
         except ValueError as _:
             tokenized_sentence.append(word)
     return ' '.join(tokenized_sentence)
